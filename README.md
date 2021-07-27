343592ef632a8eeedef1c034c37110a9c6cbee10
Anthony Vo <antvo2448@hotmail.com>
2021-07-27 19:33:24 -0400

    Multi-channel support, help command, seqMatch

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ A Discord bot for the Northeastern '24 server.
 ## Features
 
 * Private message a bot and have it output your message into a separate channel (anonymously)!
-* User verification (outside members who DM the bot will fail)
+* Alternatively, use the command directly in a Discord channel the bot has access to.
+
 * Picture support (upload the image onto Discord)
 * Embed styling (includes text, picture, and time/date)
+* Keyword detection (to prevent malicious messages)
+* Multiple channel support
+* Help command
 
 ## Screenshots
 
@@ -20,9 +24,13 @@ A Discord bot for the Northeastern '24 server.
 ![Output](pictures/botOutput.png)
 
 
-**Example Failed Message**
+**Example Help Command**
 
-![Failed Message](pictures/botVerification.png)
+![Help Command](pictures/botHelp.png)
+
+**Example Invalid Command**
+
+![Failed Message](pictures/botInvalidMessage.png)
 
 ## Running
     # For the host:
@@ -30,10 +38,11 @@ A Discord bot for the Northeastern '24 server.
 
     # For the user:
     Private message a bot: !conf (message)
+    Message in a channel the bot can see: !conf(message)
     (Attach a picture using Discord's upload system)
 
 ## Things the User Must Do
-Before running the bot, add the following 2 IDs in the bot.py file (at the top):
+Before running the bot, add the following IDs in the bot.py file (at the top):
 * Guild/Server ID (int)
 * Channel ID (int)
 * Bot Token ID (string)
--- bot.py
+++ bot.py
@@ -1,51 +1,101 @@
 from discord.ext import commands
 from datetime import datetime
 from io import BytesIO
+
+# If you want to prevent keywords from appearing in a confession, implement SequenceMatcher
+# from difflib import SequenceMatcher
+
 import discord
 
 ####################
 
 # Specific information necessary for bot to function
+
+# The Guild ID refers to the server the bot will operate in.
 # Obtain guild ID by right-clicking the server and clicking "Copy ID"
 GUILD_ID = ID_HERE # Guild ID MUST be an int, not a string!
 
+# The Channel ID refers to the specific channel the bot will be able to interact with.
 # Obtain channel ID by right-clicking the channel and clicking "Copy ID"
-CHANNEL_ID = ID_HERE #Channel ID MUST be an int, not a string!
+# Channel ID MUST be an int, not a string!
 
+# Examples of possible channels have been listed
+CONFESSIONS_ID = ID_HERE # confessions, !conf 
+VENT_ID = ID_HERE # vent, !vent
+VC_CHAT_ID = ID_HERE # vc-chat, !vc
+LGBTQ_ID = ID_HERE # lgbtq, !lgbt
+TIKTOK_ID = ID_HERE # tiktoks, !tiktok
+MEMES_ID = ID_HERE # ID_HERE, !meme
+RECOMMENDATIONS_ID = ID_HERE # recommendations, !rec
+
+# The Bot Token is necessary for the program to interact with the Discord API.
 # Obtain bot token from Discord Developer Site
 BOT_TOKEN = "ID_HERE" # Token ID MUST be a string (in quotes)!
 
 ####################
 
+# # String Sequence Comparison
+# a = 'bad phrase here'
+# seq = SequenceMatcher()
+
 # Creates the instances, uses "!" to activate commands
 bot = commands.Bot(command_prefix='!')
 
+# # sets custom status message / activity
+# @bot.event
+# async def on_ready():
+#     await bot.change_presence(activity=discord.Game(name="!confhelp"))
+
 # "!conf" command
-@bot.command()
+# Aliases are the different names that can be used to invoke a message. The alias used can be retrieved later.
+@bot.command(aliases=['vent', 'vc', 'lgbt', 'tiktok', 'meme', 'rec'])
+# @bot.command()
 async def conf(ctx, *, message=""):
 
     # Error check: skips if message has no content or image
     if (len(message) != 0) or (ctx.message.attachments != []):
 
-      # Obtains User ID and nickname to verify user is in guild/server
-      userID = (ctx.message.author.id)
-      server = bot.get_guild(GUILD_ID)
-      member = server.get_member(userID)
+      # # Error check: has "bad phrase"
+      # seq.set_seqs(a.lower(), message.lower())
+      # if seq.ratio() < 0.66:
+      # (need an else case to ignore the command)
 
-      # If member is a member of the server, they will have a name (instead of None)
-      if member != None:
         # Assigns Discord channel (given channel ID)
-        channel = bot.get_channel(CHANNEL_ID)
+        curAlias = ctx.invoked_with
+
+        # Defaults the channel (to send message to) to the confessions channel
+        # TODO: is this necessary?
+        channel = bot.get_channel(CONFESSIONS_ID)
+
+        # Checks the alias to see if the user wanted to send the message elsewhere.
+        # TODO: can this be simplified?
+        if curAlias == 'conf':
+          channel = bot.get_channel(CONFESSIONS_ID)
+        elif curAlias == 'vent':
+          channel = bot.get_channel(VENT_ID)  
+        elif curAlias == 'vc':
+          channel = bot.get_channel(VC_CHAT_ID)
+        elif curAlias == 'lgbt':
+          channel = bot.get_channel(LGBTQ_ID)
+        elif curAlias == 'tiktok':
+          channel = bot.get_channel(TIKTOK_ID)
+        elif curAlias == 'meme':
+          channel = bot.get_channel(MEMES_ID)
+        elif curAlias == 'rec':
+          channel = bot.get_channel(RECOMMENDATIONS_ID)
+        else:
+          channel = bot.get_channel(CONFESSIONS_ID)
 
         # datetime object containing current date and time
+        # the datetime object provides proper time-zone support.
         now = datetime.now()
-        currentTime = now.strftime("%d/%m %H:%M")
+        currentTime = now.strftime("%m/%d %H:%M")
 
         # Create embed message (looks better)
         embed = discord.Embed(description=message)
 
         # Set date/time as footer of embed
-        embed.set_footer(text=currentTime)
+        embed.set_footer(text=currentTime + " Ritz's Domain")
 
         # Image attachments
         files = []
@@ -62,16 +112,27 @@ async def conf(ctx, *, message=""):
         await channel.send(embed=embed)
 
         # Inform user their message has been sent
-        await ctx.send("Your message has been sent!")
+        await ctx.send("Your confession has been sent.")
 
-      # If member is not a member is the server, they will be tagged None
-      else:
-        await ctx.send("You are not a member of the server! The message was not sent.")   
+      # # Has invalid phrase
+      # else: 
+      #   await ctx.send("Warning: message contains invalid words.") 
 
     # If the message contains no image or text
     else:
-      await ctx.send("Your message does not contain any content. Message failed.")
+      await ctx.send("Warning: invalid message.")
 
+# "!confhelp" command
+@bot.command()
+async def confhelp(ctx):
+  file = discord.File("loveiswar.gif", filename="loveiswar.gif")
+
+  embed = discord.Embed(title="To use: !(channel) (msg)")
+  embed.set_author(name="Anthony Bot (Confessions) - Help")
+  embed.set_thumbnail(url="attachment://image here.png") # image goes into content root directory.
+  embed.add_field(name="Command", value="!conf\n!vent\n!vc\n!lgbt\n!tiktok\n!meme\n!rec", inline=True) # Table of commands
+  embed.add_field(name="Channel", value="[#confessions](link to channel here)", inline=True) # Add links to the channel here
+  await ctx.send(file=file, embed=embed)
 
 # Runs the bot given bot token ID
 bot.run(BOT_TOKEN)
